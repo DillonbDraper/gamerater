cf093f2002e9dedd2f010c116feacd58591d37b3
Dillon Draper <dillonbdraper@gmail.com>
2021-02-11 09:49:32 -0600

    Added data in fixtures for players/games/tokens/users, added a view and corresponding url handling for games and tested in postman.  Both list and retrieve work as intended

---
+++ raterapi/views/game.py
@@ -0,0 +1,52 @@
+from django.core.exceptions import ValidationError
+from rest_framework import status
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from raterapi.models import Game
+
+
+class Games(ViewSet):
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single game
+
+        Returns:
+            Response -- JSON serialized game instance
+        """
+        try:
+            # `pk` is a parameter to this function, and
+            # Django parses it from the URL route parameter
+            #   http://localhost:8000/games/2
+            #
+            # The `2` at the end of the route becomes `pk`
+            game = Game.objects.get(pk=pk)
+            serializer = GameSerializer(game, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def list(self, request):
+        """Handle GET requests to games resource
+
+        Returns:
+            Response -- JSON serialized list of games
+        """
+        # Get all game records from the database
+        games = Game.objects.all()
+
+        serializer = GameSerializer(
+            games, many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+class GameSerializer(serializers.ModelSerializer):
+    """JSON serializer for games
+
+    Arguments:
+        serializer type
+    """
+    class Meta:
+        model = Game
+        fields = ('id', 'title', 'description', 'designer', 'release_year', 'num_of_players', 'time_to_beat', 'esrb_rating')
